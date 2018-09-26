fb9539d2fec5ae4a9fc1ce5ae2c94a80181f9e40
Hajk <tunczorx@gmail.com>
2018-09-26 12:23:19 +0200

    Add project files.

---
+++ Codewars_Beginner_Series_Nr3 Sum_of_Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_Beginner_Series_Nr3_Sum_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
